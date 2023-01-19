1f145063d321f28f501ece54448a062c482216bb
clia <clia@163.com>
2023-01-19 15:29:06 +0800

    Initial version.

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+## 0.0.1
+
+* Initial Release
+
+## 0.0.2
+* Fix Some Bug
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,37 +1,5 @@
-<!-- 
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
-
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/guides/libraries/writing-package-pages). 
-
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-library-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/developing-packages). 
--->
-
-Insert Material Icon and FontAwesome icons dynamically in Flutter app when the icons are not known at compile time.
-
-The version of font_awesome icons is: 6.1.0
-
-## Features
-
-<!-- TODO: List what your package can do. Maybe include images, gifs, or videos. -->
-
-## Getting started
-
-<!-- TODO: List prerequisites and provide or point to information on how to -->
-<!-- start using the package. -->
-
-## Usage
-
-Add clia_dynamic_icons to pubspec.yaml
-All icons namse should be in the same format you can find on https://api.flutter.dev/flutter/material/Icons-class.html and fontawesome.com  (e.g. add)
-
-```dart
 import 'package:flutter/material.dart';
-import 'package:clia_dynamic_icons/dynamic_icons.dart';
+import 'package:dynamic_icons/dynamic_icons.dart';
 
 void main() {
   runApp(const MyApp());
@@ -39,12 +7,22 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Dynamic Icon',
       theme: ThemeData(
-
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Dynamic Icon Example'),
@@ -55,6 +33,17 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -64,7 +53,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
 
     var iconList = [
       {
@@ -106,7 +100,9 @@ class _MyHomePageState extends State<MyHomePage> {
     ];
     return Scaffold(
       appBar: AppBar(
-        title: Text("Dynamic Icons"),
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
       body: ListView.builder(
         itemCount: iconList.length,
@@ -119,15 +115,7 @@ class _MyHomePageState extends State<MyHomePage> {
             );
           }
       ),
-
+       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
-```
-
-## Additional information
-
-References
-  https://pub.dev/packages/font_awesome_flutter
-
-
